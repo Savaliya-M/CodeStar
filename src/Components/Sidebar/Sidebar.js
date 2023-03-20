6430dc58d9b5fc4b0af74b48224d37a74502ebc6
Savaliya-M <masavaliya183@gmail.com>
2023-03-20 23:29:59 +0530

    Add link in sidebar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { RiBracesFill } from "react-icons/ri";
+import { GrCertificate } from "react-icons/gr";
+import { BsTrophy } from "react-icons/bs";
+import { MdOutlineQuestionAnswer, MdOutlineKeyboardArrowUp, MdOutlineKeyboardArrowDown } from "react-icons/md";
+
+import './sidebar.css'
 
 const Sidebar = () => {
     return (
         <>
-            <div className='sidebar'>
-                <div className="logo">
-                    <h1 className='text-2xl text-center'>CODESTAR</h1>
+            <div className='sidebar flex flex-col'>
+                <div className="logo ">
+                    <h1 className='text-2xl text-center'>CODESTAR </h1>
                 </div>
-
+                <div className='links'>
+                    <div>
+                        <div className='ruler'>
+                        </div>
+                        <div className='link-icons'>
+                            <RiBracesFill />
+                        </div>
+                        <div className='link-text'>
+                            <Link to="/">Home</Link>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='link-icons'>
+                            <MdOutlineQuestionAnswer />
+                        </div>
+                        <div className='link-text'>
+                            Questions
+                            <MdOutlineKeyboardArrowDown />
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </>
