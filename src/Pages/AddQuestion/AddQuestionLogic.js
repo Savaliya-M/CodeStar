9ed5bb22a8c6f2315b3e26db4a5fb85637dae64e
Savaliya-M <masavaliya183@gmail.com>
2023-05-25 23:52:29 +0530

    working on error on question add page

--- src/Pages/AddQuestion/AddQuestionLogic.js
+++ src/Pages/AddQuestion/AddQuestionLogic.js
@@ -3,20 +3,21 @@ import { useEffect, useState } from "react";
 const AddQuestionLogic = () => {
   const [questionText, setQuestionText] = useState("");
   const [code, setCode] = useState("");
-  const [questionData, setQuestionData] = useState({
+  let [questionData, setQuestionData] = useState({
     questionDesc: "",
     questionCode: "",
     questionHead: "",
     level: "",
     requiredSkill: "",
     maxScore: "",
-    TestCases: [],
+    TestCases: [{ testCaseId: "", Input: "", Output: "" }],
   });
-  const [testCase, setTestCase] = useState({
-    testCaseId: "",
-    Input: "",
-    Output: "",
-  });
+
+  // const [testCase, setTestCase] = useState({
+  //   testCaseId: "",
+  //   Input: "",
+  //   Output: "",
+  // });
 
   useEffect(() => {
     console.log(questionData);
